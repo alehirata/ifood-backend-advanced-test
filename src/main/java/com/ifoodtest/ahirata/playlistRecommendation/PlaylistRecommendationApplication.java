7414e0b4f59fae9bc9b6d4f0bacf3fffa1d36ac4
Alexandre Hirata <alexandre.hirata@gmail.com>
2017-11-26 14:36:10 -0200

    Added Spring Initalizr raw project

---
+++ src/main/java/com/ifoodtest/ahirata/playlistRecommendation/PlaylistRecommendationApplication.java
@@ -0,0 +1,12 @@
+package com.ifoodtest.ahirata.playlistRecommendation;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PlaylistRecommendationApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PlaylistRecommendationApplication.class, args);
+	}
+}
